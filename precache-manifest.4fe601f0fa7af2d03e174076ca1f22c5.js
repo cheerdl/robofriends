1ab53c31a25cf01bc97acc6adef7734aceb942d5
cheer <cheereieisk@gmail.com>
2020-03-19 23:25:59 +0700

    Updates

---
+++ precache-manifest.4fe601f0fa7af2d03e174076ca1f22c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143d0a1fcbaf91991561707394869578",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c424d4dd9fa80c3b2fe",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1c07ce90d3e4b4758ca9",
+    "url": "/robofriends/static/css/main.326f8a29.chunk.css"
+  },
+  {
+    "revision": "0c424d4dd9fa80c3b2fe",
+    "url": "/robofriends/static/js/2.0761117a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.0761117a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c07ce90d3e4b4758ca9",
+    "url": "/robofriends/static/js/main.fbb4101f.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
